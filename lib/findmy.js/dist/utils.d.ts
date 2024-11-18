75ab23ad36b948163f90c0cb74b29e8fd04eb651
Martijn Poppen <m.poppen@ziggo.nl>
2024-11-18 15:43:09 +0100

    OPT: ipv4

--- lib/findmy.js/dist/utils.d.ts
+++ lib/findmy.js/dist/utils.d.ts
@@ -1,7 +1,11 @@
 import { Cookie } from 'tough-cookie';
 import { Response } from 'node-fetch';
+import https from 'https';
 declare const stringToU8Array: (str: string) => Uint8Array;
 declare const base64ToU8Array: (str: string) => Uint8Array;
 export { base64ToU8Array, stringToU8Array };
 export declare function extractiCloudCookies(response: Response): Cookie[];
+export declare const fetchOptions: {
+    agent: https.Agent;
+};
 //# sourceMappingURL=utils.d.ts.map
