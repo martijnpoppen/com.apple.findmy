1f78ac8c12adbefb67afc92713d7ded187e3f63d
Martijn Poppen <m.poppen@ziggo.nl>
2026-07-24 16:12:32 +0200

    FIX: auth

--- lib/findmy.js/dist/findmy-authentication.d.ts
+++ lib/findmy.js/dist/findmy-authentication.d.ts
@@ -4,16 +4,5 @@ export interface AuthenticatedData {
     cookies: CookieJar;
     accountInfo: iCloudAccountInfo;
 }
-export declare function collectSetCookie(res: {
-    headers: {
-        raw: () => {
-            (): any;
-            new (): any;
-            [x: string]: any;
-        };
-        getSetCookie: () => any;
-    };
-}): string;
-export declare function sessionHeaders(res: any): Record<string, string>;
 export declare function AuthenticateFindMy(username: string, password: string): Promise<AuthenticatedData>;
 //# sourceMappingURL=findmy-authentication.d.ts.map
